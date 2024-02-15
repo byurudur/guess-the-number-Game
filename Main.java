06009f2711d7b336c43fd7a3f1bbcd806a825171
Batuhan Yürüdür <batuhanyurudur@gmail.com>
2024-02-15 14:23:40 +0300

    Codes cleared and bug-checked.

--- Main.java
+++ Main.java
@@ -10,11 +10,14 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         // Code chooses a random number and user tries to guess.
+
 
         // We need to call the Scanned and Random classes.
         Scanner input = new Scanner(System.in);
         Random rand= new Random();
+
 
         // Explaining the game to the user.
         System.out.println("Are you a fortune teller? Prove it!");
@@ -22,13 +25,14 @@ public class Main {
         System.out.println("----------------------------------");
         System.out.println("Enter your guess now: ");
 
+
         // Detecting variables
         int randomNumber, guess;
         randomNumber = rand.nextInt(51);
         guess = input.nextInt();
 
-        // Starting the game with while loop.
 
+        // Starting the game with while loop.
         while (true){
             if (randomNumber == guess){
                 System.out.println("You NAILED IT! Number is " + randomNumber);
